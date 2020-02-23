bb41b2c9e0dfcd346db87568bf03f8e69b118a12
Anna234365 <6054404@gmail.com>
2020-02-23 20:39:10 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  return Math.min.apply(null, array) == Infinity ? 0 : Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  return Math.max.apply(null, array) == Infinity || Math.max.apply(null, array) == -Infinity ? 0 : Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array.length == 0) {
+    return 0;
+  } else {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+    }
+    return sum/array.length;
+  }
 }
+
+  
+  
+
+
